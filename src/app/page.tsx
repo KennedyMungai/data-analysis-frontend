b9abc32fd36e8021433ba17a78848991260c9990
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-17 14:44:38 +0300

    Made some changes to the index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import VideoBackground from '@/components/video-background'
+import { cn } from '@/lib/utils'
+import BackgroundVideo from 'next-video/background-video'
+import { Poppins } from 'next/font/google'
+import Image from 'next/image'
+import bg_video from '/videos/bg_video.mp4'
+
+const font = Poppins({ subsets: ['latin'], weight: '600' })
 
 const HomePage = () => {
 	return (
-		<main className='h-full bg-transparent'>
-			<VideoBackground />
+		<main className='flex items-center justify-center w-full h-full text-white bg-transparent'>
+			<BackgroundVideo src={bg_video}>
+				<div className='bg-neutral-200/20 rounded-xl gap-x-8 flex items-center px-32 py-16'>
+					<Image
+						src='/icon.svg'
+						width={100}
+						height={100}
+						alt='Logo'
+					/>
+					<p className={cn('font-bold text-4xl', font.className)}>
+						Data Analysis
+					</p>
+				</div>
+			</BackgroundVideo>
 		</main>
 	)
 }
