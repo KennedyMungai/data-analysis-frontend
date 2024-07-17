eb1f34babf44e7d34885516e18a6c4a9e052cbac
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-17 15:57:23 +0300

    Created the query provider for the application

---
+++ src/providers/query-provider.tsx
@@ -0,0 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactNode } from 'react'
+
+type Props = {
+	children: ReactNode
+}
+
+const QueryProvider = ({ children }: Props) => {
+	const queryClient = new QueryClient()
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	)
+}
+
+export default QueryProvider
