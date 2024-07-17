c80f9175cbe1bbc47926fdb318df35d2fa8e5ab4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-17 14:51:29 +0300

    Added a link to the dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
+import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import BackgroundVideo from 'next-video/background-video'
 import { Poppins } from 'next/font/google'
 import Image from 'next/image'
+import Link from 'next/link'
 import bg_video from '/videos/bg_video.mp4'
-import { Button } from '@/components/ui/button'
 
 const font = Poppins({ subsets: ['latin'], weight: '600' })
 
 const HomePage = () => {
 	return (
 		<main className='flex items-center justify-center w-full h-full text-white bg-transparent'>
-			<BackgroundVideo src={bg_video}>
+			<BackgroundVideo src={bg_video} loop autoPlay>
 				<div className='bg-neutral-200/20 rounded-xl flex flex-col items-center justify-center px-32 py-16'>
 					<div className='gap-x-8 flex items-center'>
 						<Image
@@ -23,13 +24,15 @@ const HomePage = () => {
 							Data Analysis
 						</p>
 					</div>
-					<Button
-						variant={'outline'}
-						size={'lg'}
-						className='text-xl bg-transparent'
-					>
-						Get Started
-					</Button>
+					<Link href='/dashboard'>
+						<Button
+							variant={'outline'}
+							size={'lg'}
+							className='text-xl bg-transparent'
+						>
+							Get Started
+						</Button>
+					</Link>
 				</div>
 			</BackgroundVideo>
 		</main>
