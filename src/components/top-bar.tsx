2b37bb8e75337328b6c7cf3e361e906bd36c8030
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-17 15:39:36 +0300

    Made some changes to the topbar component

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,7 +1,15 @@
-type Props = {}
+type Props = {
+	title: string
+}
 
-const TopBar = () => {
-	return <div className='w-full h-16 border-b shadow-sm'>TopBar</div>
+const TopBar = ({ title }: Props) => {
+	return (
+		<div className='flex items-center justify-between w-full h-16 p-2 border-b shadow-sm'>
+			<p className='text-2xl uppercase'>{title}</p>
+			{/* TODO: Add user auth shenanigans */}
+			<p>{new Date().toLocaleDateString()}</p>
+		</div>
+	)
 }
 
 export default TopBar
