e133fc37baa027df45d212977e65fafe3f23f787
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-17 16:17:16 +0300

    Made some changes

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -7,7 +7,7 @@ const TopBar = ({ title }: Props) => {
 		<div className='flex items-center justify-between w-full h-16 p-2 border-b shadow-sm'>
 			<p className='text-2xl uppercase'>{title}</p>
 			{/* TODO: Add user auth shenanigans */}
-			<p>{new Date().toLocaleDateString()}</p>
+			<div></div>
 		</div>
 	)
 }
