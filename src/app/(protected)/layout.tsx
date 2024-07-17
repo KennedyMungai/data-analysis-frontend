fd60334974f7c2c8ff435e46c2ff12626254c7bd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-17 15:28:04 +0300

    Made some updates to the layout component

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,3 +1,4 @@
+import SideBar from '@/components/sidebar'
 import TopBar from '@/components/top-bar'
 import { ReactNode } from 'react'
 
@@ -7,10 +8,15 @@ type Props = {
 
 const ProtectedLayout = ({ children }: Props) => {
 	return (
-		<main className='h-full'>
-			<TopBar />
-			{children}
-		</main>
+		<div className='flex w-full h-full'>
+			<div className='w-32 h-full'>
+				<SideBar />
+			</div>
+			<main className='w-full h-full'>
+				<TopBar />
+				{children}
+			</main>
+		</div>
 	)
 }
 
