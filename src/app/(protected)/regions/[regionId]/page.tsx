009f68676592eca4b8ee7e5802b1ef43a8fd0f2e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-18 14:37:12 +0300

    Added the markup and logic to the individual region page

--- src/app/(protected)/regions/[regionId]/page.tsx
+++ src/app/(protected)/regions/[regionId]/page.tsx
@@ -2,7 +2,9 @@
 
 import SummaryCard from '@/components/summary-card'
 import TopBar from '@/components/top-bar'
+import { Button } from '@/components/ui/button'
 import { useFetchRegion } from '@/features/regions/api/use-fetch-region'
+import Link from 'next/link'
 
 type Props = {
 	params: {
@@ -37,11 +39,31 @@ const IndividualRegionPage = ({ params: { regionId } }: Props) => {
 		<div className='h-full'>
 			<TopBar title={region.region_name} />
 			<div className='h-full p-4'>
+				<div className='flex justify-around pb-2'>
+					<div>
+						{/* TODO: Add a calendar for specifying the time frame of the data being analyzed */}
+					</div>
+					<div>
+						<Link
+							href={`http://127.0.0.1:8000/stores/region/${regionId}`}
+						>
+							<Button
+								size={'lg'}
+								variant={'outline'}
+								className='text-lg bg-transparent'
+							>
+								Stores
+							</Button>
+						</Link>
+					</div>
+					<div />
+				</div>
 				<div className='flex justify-between'>
 					<SummaryCard label='Total Amount' amount={0} />
 					<SummaryCard label='Total Number of Incidents' amount={0} />
 					<SummaryCard label='Total ' amount={0} />
 				</div>
+				{/* TODO: Add the charts and tables for analysis */}
 			</div>
 		</div>
 	)
