2cdd0ba3c2a6a96f94d6cf0f1e9ef0c1627c8a20
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-18 11:06:41 +0300

    Made some updates

--- src/app/(protected)/regions/page.tsx
+++ src/app/(protected)/regions/page.tsx
@@ -1,10 +1,54 @@
+'use client'
+
+import InfoCard from '@/components/info-card'
 import TopBar from '@/components/top-bar'
+import { useFetchRegions } from '@/features/regions/api/use-fetch-regions'
 
 const RegionsPage = () => {
+	const { data, isPending, isError } = useFetchRegions()
+
+	if (isError) {
+		return (
+			<div className='h-full'>
+				<TopBar title={'Regions'} />
+				<div className='flex flex-wrap items-center justify-center h-full p-4'>
+					<p className='text-rose-500 text-4xl font-bold uppercase'>
+						Something Went Wrong!
+					</p>
+				</div>
+			</div>
+		)
+	}
+
+	// TODO: Add the Skeletons for the isPending state
+	if (isPending) {
+		return (
+			<div className='h-full'>
+				<TopBar title={'Regions'} />
+				<div className='flex flex-wrap items-center justify-center h-full p-4'>
+					Loading...
+				</div>
+			</div>
+		)
+	}
+
+	const regions: Region[] = .data
+
 	return (
-		<div>
+		<div className='h-full'>
 			<TopBar title={'Regions'} />
-			RegionsPage
+			<div className='flex flex-wrap items-center justify-center h-full p-4'>
+				{regions.map((region) => (
+					<InfoCard
+						link={`/regions/${region.region_id}`}
+						title={`${region.region_name}`}
+						key={region.region_id}
+					>
+						Some Info
+						{/* TODO: Add data specific to a region */}
+					</InfoCard>
+				))}
+			</div>
 		</div>
 	)
 }
