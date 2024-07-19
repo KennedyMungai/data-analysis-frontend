de7f01e6ec35aa51ff01c7c814c7fdc32688f5d1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-19 15:40:17 +0300

    Created the hook to fetch the data from a single store section

---
+++ src/features/store-sections/api/use-fetch-single-store-section.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+export const useFetchSingleStoreSection = (storeSectionId: string) =>
+	useQuery({
+		queryKey: ['store-section', { storeSectionId }],
+		queryFn: async () =>
+			axios
+				.get(`http://localhost:8000/store_sections/${storeSectionId}`)
+				.then((res) => res.data as IStoreSection)
+	})
