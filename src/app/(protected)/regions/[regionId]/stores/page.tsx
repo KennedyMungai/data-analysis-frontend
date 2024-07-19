fe0fb2ef8747530cc837ff83ad6a4f931fa9f6e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-19 11:33:38 +0300

    Added a scrollview for the cards on the page

--- src/app/(protected)/regions/[regionId]/stores/page.tsx
+++ src/app/(protected)/regions/[regionId]/stores/page.tsx
@@ -2,6 +2,7 @@
 
 import InfoCard from '@/components/info-card'
 import TopBar from '@/components/top-bar'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { useFetchRegion } from '@/features/regions/api/use-fetch-region'
 import { useFetchRegionStores } from '@/features/stores/api/use-fetch-region-stores'
 import AddStoreCard from '@/features/stores/components/add-store-card'
@@ -31,19 +32,21 @@ const StoresPage = () => {
 	return (
 		<div className='h-full'>
 			<TopBar title={regionQuery.data?.region_name || ''} />
-			<div className='gap-x-4 gap-y-8 flex flex-wrap items-center justify-center h-full p-4'>
-				{regionStoresQuery.data?.map((store) => (
-					<InfoCard
-						link={`/regions/${regionId}/stores/${store.store_id}`}
-						title={store.store_name}
-						key={store.store_id}
-					>
-						Some Info
-						{/* TODO: Add data specific to a region */}
-					</InfoCard>
-				))}
-				<AddStoreCard regionId={regionId} />
-			</div>
+			<ScrollArea className='h-full w-full px-12 py-6'>
+				<div className='gap-x-4 gap-y-8 flex flex-wrap items-center justify-center h-full p-4'>
+					{regionStoresQuery.data?.map((store) => (
+						<InfoCard
+							link={`/regions/${regionId}/stores/${store.store_id}`}
+							title={store.store_name}
+							key={store.store_id}
+						>
+							Some Info
+							{/* TODO: Add data specific to a region */}
+						</InfoCard>
+					))}
+					<AddStoreCard regionId={regionId} />
+				</div>
+			</ScrollArea>
 		</div>
 	)
 }
