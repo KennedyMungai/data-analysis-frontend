5fe443c7dd458760b9197b072956edff00b84a36
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-19 10:27:00 +0300

    Created the sheet provider component

---
+++ src/providers/sheet-provider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useMountedState } from 'react-use'
+
+const SheetProvider = () => {
+	const isMounted = useMountedState()
+
+	if (!isMounted) return null
+
+	return <></>
+}
+
+export default SheetProvider
