657c520f487f2ae84dc61032c52ff93ce36c70d8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-19 16:16:42 +0300

    Added toaster confirmations for when an incident is created

--- src/features/incidents/api/use-create-incident.ts
+++ src/features/incidents/api/use-create-incident.ts
@@ -1,11 +1,14 @@
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
+import { toast } from 'sonner'
 
-export const useCreateIncident = (incidentData: ICreateIncident) =>
+export const useCreateIncident = () =>
 	useMutation({
 		mutationKey: ['create-incident'],
-		mutationFn: async () =>
+		mutationFn: async (incidentData: ICreateIncident) =>
 			axios
 				.post('http://localhost:8000/incidents', incidentData)
-				.then((res) => res.data as IIncident)
+				.then((res) => res.data as IIncident),
+		onSuccess: () => toast.success('Incident Created!'),
+		onError: () => toast.error('Failed to Create Incident!')
 	})
