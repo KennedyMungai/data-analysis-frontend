5fcb48478fa4c1a591f66048b4dd413f1f992b1b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-19 15:46:30 +0300

    Added an add incident button

--- src/app/(protected)/regions/[regionId]/stores/[storeId]/storeSections/[storeSectionId]/page.tsx
+++ src/app/(protected)/regions/[regionId]/stores/[storeId]/storeSections/[storeSectionId]/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import SummaryCard from '@/components/summary-card'
 import TopBar from '@/components/top-bar'
+import { Button } from '@/components/ui/button'
 import { useFetchSingleStoreSection } from '@/features/store-sections/api/use-fetch-single-store-section'
 import { useFetchSingleStore } from '@/features/stores/api/use-fetch-single-store'
 import { usePathname } from 'next/navigation'
@@ -51,6 +53,26 @@ const StoreSectionDetails = ({ params: { storeSectionId } }: Props) => {
 			<TopBar
 				title={`${store.store_name} ${storeSection.store_section_name}`}
 			/>
+			<div className='flex justify-around py-2'>
+				<div>
+					{/* TODO: Add a calendar for specifying the time frame of the data being analyzed */}
+				</div>
+				<div>
+					<Button
+						size={'lg'}
+						variant={'outline'}
+						className='text-lg bg-transparent'
+					>
+						Add Incident
+					</Button>
+				</div>
+				<div />
+			</div>
+			<div className='flex justify-between'>
+				<SummaryCard label='Total Amount' amount={0} />
+				<SummaryCard label='Total Number of Incidents' amount={0} />
+				<SummaryCard label='Total ' amount={0} />
+			</div>
 		</div>
 	)
 }
