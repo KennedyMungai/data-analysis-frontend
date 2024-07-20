944ca18dc585228ccf40433717c12cf7302f8cca
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-20 11:20:56 +0300

    Added a date range picker for the calendar

--- src/app/(protected)/overall/page.tsx
+++ src/app/(protected)/overall/page.tsx
@@ -1,16 +1,34 @@
+'use client'
+
 import SummaryCard from '@/components/summary-card'
 import TopBar from '@/components/top-bar'
 import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { addDays } from 'date-fns'
 import Link from 'next/link'
+import { useState } from 'react'
+import { DateRange } from 'react-day-picker'
 
 const OverallDataPage = () => {
+	const initialRange: DateRange = {
+		from: new Date(),
+		to: addDays(new Date(), 4)
+	}
+
+	const [range, setRange] = useState<DateRange | undefined>(initialRange)
+
 	return (
 		<div className='h-full p-2'>
 			<TopBar title={'Overall Data'} />
 			<div className='h-full p-4'>
 				<div className='flex justify-around pb-2'>
 					<div>
-						{/* TODO: Add a calendar for specifying the time frame of the data being analyzed */}
+						<Calendar
+							mode='range'
+							selected={range}
+							onSelect={setRange}
+							className='rounded-md border'
+						/>
 					</div>
 					<div>
 						<Link href={'/overall/regions'}>
