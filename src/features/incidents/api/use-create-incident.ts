5e1761597bf48265a8d1d8dcb22abcb86a99d22a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-26 15:24:28 +0300

    Invalidating queries once a new incident is created

--- src/features/incidents/api/use-create-incident.ts
+++ src/features/incidents/api/use-create-incident.ts
@@ -1,14 +1,22 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import { toast } from 'sonner'
 
-export const useCreateIncident = () =>
-	useMutation({
+export const useCreateIncident = () => {
+	const queryClient = useQueryClient()
+
+	const mutation = useMutation({
 		mutationKey: ['create-incident'],
 		mutationFn: async (incidentData: ICreateIncident) =>
 			axios
 				.post('http://localhost:8000/incidents', incidentData)
 				.then((res) => res.data as IIncident),
-		onSuccess: () => toast.success('Incident Created!'),
+		onSuccess: () => {
+			toast.success('Incident Created!')
+			queryClient.invalidateQueries({ queryKey: ['incidents'] })
+		},
 		onError: () => toast.error('Failed to Create Incident!')
 	})
+
+	return mutation
+}
