9a0e3af1ab5ce1aa71277f8bcc075825d02b93fa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-27 11:13:40 +0300

    Added a countup component to the summary card

--- src/components/summary-card.tsx
+++ src/components/summary-card.tsx
@@ -1,8 +1,9 @@
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import CountUp from 'react-countup'
 
 type Props = {
 	label: string
-	amount: string
+	amount: number
 }
 
 const SummaryCard = ({ amount, label }: Props) => {
@@ -11,8 +12,8 @@ const SummaryCard = ({ amount, label }: Props) => {
 			<CardHeader className='text-3xl font-semibold text-center'>
 				{label}
 			</CardHeader>
-			<CardContent className='text-muted-foreground text-lg text-center'>
-				{amount}
+			<CardContent className='text-muted-foreground text-lg text-center font-semibold'>
+				<CountUp end={amount} />
 			</CardContent>
 		</Card>
 	)
